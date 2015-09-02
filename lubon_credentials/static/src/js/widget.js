df1dcbad47be07de32b3156aead5716d4a179778
Nedas Žilinskas <nedas.zilinskas@gmail.com>
2015-09-02 16:59:32 +0300

    clipboard workaround via swf

--- lubon_credentials/static/src/js/widget.js
+++ lubon_credentials/static/src/js/widget.js
@@ -5,16 +5,19 @@ openerp.lubon_credentials = function(openerp) {
         template: "credentials",
         events: {
             'click .reveal_password_button': 'reveal_password',
-            'click .reveal_password_content_copy': 'copy_to_clipboard',
         },
         start: function() {
+            var self = this;
+
             this.credentials_id = this.get('value');
             this.set('value', '');
             this.reveal_password();
+
             return this._super();
         },
         reveal_password: function() {
             var self = this;
+
             var model = new openerp.web.Model('lubon_credentials.credentials');
 
             var content_element = self.$el.find('.reveal_password_content');
@@ -31,6 +34,17 @@ openerp.lubon_credentials = function(openerp) {
                 content_element.show();
                 button_element.hide();
                 pin_element.hide();
+                var copy_sel = content_element.find('.reveal_password_content_copy');
+                copy_sel.on('click', function(e) {
+                    e.preventDefault();
+                });
+                copy_sel.clipboard({
+                    path: '/lubon_credentials/static/src/swf/jquery.clipboard.swf',
+                    copy: function() {
+                        var value = copy_sel.parents('.reveal_password_content:first').find('.reveal_password_content_value').text();
+                        return value;
+                    }
+                });
                 setTimeout(function() {
                     content_element.find('.reveal_password_content_value').text('********');
                     content_element.find('.reveal_password_content_copy').hide();
@@ -43,37 +57,32 @@ openerp.lubon_credentials = function(openerp) {
                 button_element.show();
                 pin_element.show();
             });
-        },
-        copy_to_clipboard: function(e) {
-            e.preventDefault();
-            copy_to_clipboard($(e.target).prev('.reveal_password_content_value'))
         }
     });
 
     openerp.web.form.widgets.add('clipboard', 'openerp.lubon_credentials.Clipboard');
     openerp.lubon_credentials.Clipboard = openerp.web.form.FieldChar.extend({
         template: "FieldCharClipboard",
-        events: {
-            'click .content_copy': 'copy_to_clipboard',
-        },
         init: function(field_manager, node) {
             this.clipboard = true;
             return this._super(field_manager, node);
         },
         start: function() {
+            var self = this;
+
+            var copy_sel = self.$el.find('.content_copy');
+            copy_sel.on('click', function(e) {
+                e.preventDefault();
+            });
+            copy_sel.clipboard({
+                path: '/lubon_credentials/static/src/swf/jquery.clipboard.swf',
+                copy: function() {
+                    var value = copy_sel.prev('.oe_form_char_content').text();
+                    return value;
+                }
+            });
+
             return this._super();
-        },
-        copy_to_clipboard: function(e) {
-            e.preventDefault();
-            copy_to_clipboard($(e.target).prev('.oe_form_char_content'))
         }
     });
-
-    function copy_to_clipboard(element) {
-        var $temp = $("<input>")
-        $("body").append($temp);
-        $temp.val($(element).text()).select();
-        document.execCommand("copy");
-        $temp.remove();
-    }
 }
