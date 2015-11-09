2348d63ac930e79277bca370da850780bb6844b9
Luc Bonjean <luc@lubon.be>
2015-11-09 13:59:45 +0100

    Add update hertsens security

--- hertsens_base/__openerp__.py
+++ hertsens_base/__openerp__.py
@@ -17,10 +17,10 @@
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.2',
+    'version': '0.3',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','account'],
+    'depends': ['base','account', 'account_cancel'],
 
     # always loaded
     'data': [
--- lubon_project/__openerp__.py
+++ lubon_project/__openerp__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'project'],
+    'depends': ['base', 'project', ],
 
     # always loaded
     'data': [
