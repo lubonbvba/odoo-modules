76d50904be633ce6b727e3d53a6959d59aa3bc56
Luc Bonjean <luc@lubon.be>
2016-02-21 13:24:19 +0100

    Small changes qlan

--- lubon_qlan/__openerp__.py
+++ lubon_qlan/__openerp__.py
@@ -17,7 +17,7 @@
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
     'category': 'Lubon customer',
-    'version': '0.2',
+    'version': '8.0.0.3.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base','pad', 'analytic','stock','hr_timesheet_invoice', 'lubon_base','lubon_credentials', 'lubon_partners'],
