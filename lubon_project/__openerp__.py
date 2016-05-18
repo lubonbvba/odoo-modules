6982f5fb89ec177f035842b9b9905e5d4055fcf8
Luc Bonjean <luc@lubon.be>
2016-05-18 12:32:43 +0200

    small modif

--- lubon_project/__openerp__.py
+++ lubon_project/__openerp__.py
@@ -17,7 +17,7 @@
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
     'category': 'Lubon',
-    'version': '8.0.0.12.0',
+    'version': '8.0.0.14.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'project','pad'],
