6c2d7ef0f43e466925cb63ee20dbe76b973bad6d
Luc Bonjean <luc@lubon.be>
2016-09-03 15:04:21 +0200

    Small update

--- lubon_qlan/models/restore_points.py
+++ lubon_qlan/models/restore_points.py
@@ -46,11 +46,14 @@ class lubon_qlan_restore_points_stats(models.Model):
 	number_target=fields.Integer()
 	rate_succeeded=fields.Integer(string="Succeeded (%)")
 	number_requests_failed=fields.Integer(help="Number of requests where the veeam api returned an error result. The backup result is unknown.")
+	re_evaluate=fields.Boolean(help="Refresh in next batch job?")
 	@api.multi
 	def process_restorepoints_stats(self,dummy=None):
 		date=datetime.strptime(fields.Date.today(), "%Y-%m-%d").date() + timedelta(days=-1)
 		date=datetime.strftime(date, "%Y-%m-%d")
-		self.generate_restorepoints_instances(date).check_all_restorepoints()
+		self.generate_restorepoints_instances(date).re_evaluate=True
+		for stats in self.search([('re_evaluate','=', True)]):
+			stats.check_all_restorepoints()
 
 
 
@@ -84,6 +87,7 @@ class lubon_qlan_restore_points_stats(models.Model):
 		self.number_requests_failed=self.env['lubon_qlan.restorepoints_instances'].search_count([('stats_id','=',self.id),('result_code',"!=",200)])
 
 		self.rate_succeeded=int(self.number_succeeded*100/len(self.restorepoints_instances_ids))
+		self.re_evaluate=False
 		#pdb.set_trace()	
 
 	def init(self,cr):
@@ -94,12 +98,12 @@ class lubon_qlan_restore_points_stats(models.Model):
 		#pdb.set_trace()
 	@api.multi	
 	def _migrate(self):
-		if 1==1:
+		if 1==12:
 	# update vm's
 			vmlist=self.env['lubon_qlan.assets'].search([('asset_name', 'like', 'replica'),('asset_type',"=",'vm')])
 			for vm in vmlist:
 				vm.vm_check_backup=False
-		if 1==2:		
+		if 1==12:		
 	# create older restore points
 			date ="2016-08-01"
 			while date < fields.Date.today():
