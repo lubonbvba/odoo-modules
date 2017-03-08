47ae108733d5bf990261bd951c5198bc100fc299
Luc Bonjean <luc@lubon.be>
2017-03-08 20:52:16 +0100

    Added vm backup config menu

--- lubon_qlan/__openerp__.py
+++ lubon_qlan/__openerp__.py
@@ -17,7 +17,7 @@
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
     'category': 'Lubon customer',
-    'version': '8.0.0.26.0',
+    'version': '8.0.0.27.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base','pad', 'analytic','stock','hr_timesheet_invoice', 'lubon_base','lubon_credentials', 'lubon_partners'],
@@ -30,6 +30,7 @@
         'views/lubon_qlan.xml',
         'views/lubon_invoicing.xml',
         'views/restorepoints.xml',
+        'views/lubon_qlan_vm_backup_config.xml',
         'reports/lubon_qlan_invoice.xml',
         'reports/restore_points.xml',
         'data/lubon_qlan_data.xml',
