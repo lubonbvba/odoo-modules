6b6203ddae69801ae45fcec3bd3bcb0d81ef2a97
Luc Bonjean <luc@lubon.be>
2023-03-06 09:53:34 +0100

    Modified next report date, removed billing history stuff

--- lubon_contracts/models/lubon_contracts.py
+++ lubon_contracts/models/lubon_contracts.py
@@ -3,59 +3,59 @@
 from openerp import models, fields, api
 import pdb
 
-class lubon_qlan_billing_history(models.Model):
-	_name = 'lubon_qlan.billing_history'
-	_description = 'Billing detail lines'
-	_sql_constraints = [('model_and_id','UNIQUE(related_model,related_id)','Related model/related id needs to be unique')]
-	related_model=fields.Char(help="Model that originated this line")
-	related_id=fields.Integer()
-	related_user_model=fields.Char(help="Name of the user model that relates to this entry, eg office 365, active directory")
-	related_user_id=fields.Integer()	
-	contract_line_id=fields.Many2one('account.analytic.invoice.line')
-	contract_id=fields.Many2one('account.analytic.account')
-	active=fields.Boolean(default=True)
-	date_start=fields.Datetime()
-	date_end=fields.Datetime()
-	description=fields.Char()
-	owner=fields.Char(help='User/device that is used to group by')
+# class lubon_qlan_billing_history(models.Model):
+# 	_name = 'lubon_qlan.billing_history'
+# 	_description = 'Billing detail lines'
+# 	_sql_constraints = [('model_and_id','UNIQUE(related_model,related_id)','Related model/related id needs to be unique')]
+# 	related_model=fields.Char(help="Model that originated this line")
+# 	related_id=fields.Integer()
+# 	related_user_model=fields.Char(help="Name of the user model that relates to this entry, eg office 365, active directory")
+# 	related_user_id=fields.Integer()	
+# 	contract_line_id=fields.Many2one('account.analytic.invoice.line')
+# 	contract_id=fields.Many2one('account.analytic.account')
+# 	active=fields.Boolean(default=True)
+# 	date_start=fields.Datetime()
+# 	date_end=fields.Datetime()
+# 	description=fields.Char()
+# 	owner=fields.Char(help='User/device that is used to group by')
 
 
-	@api.multi
-	def checkline(self):
-		#check if the referring record still exists.
-		if not self.env[self.related_model].browse(self.related_id).exists():
-			self.unlink()
+# 	@api.multi
+# 	def checkline(self):
+# 		#check if the referring record still exists.
+# 		if not self.env[self.related_model].browse(self.related_id).exists():
+# 			self.unlink()
 
-	@api.multi
-	def verify_billing_history_line(self,related,number,contract_line_id,description,related_user=None,owner=None):
+# 	@api.multi
+# 	def verify_billing_history_line(self,related,number,contract_line_id,description,related_user=None,owner=None):
 		
-		current_line=self.search([('related_model','ilike', str(related._model) ),('related_id','=',related.id)])
-		if not current_line:
-			current_line=self.create({
-				'related_model': str(related._model),
-				'related_id': related.id,
-				'contract_line_id': contract_line_id.id,
+# 		current_line=self.search([('related_model','ilike', str(related._model) ),('related_id','=',related.id)])
+# 		if not current_line:
+# 			current_line=self.create({
+# 				'related_model': str(related._model),
+# 				'related_id': related.id,
+# 				'contract_line_id': contract_line_id.id,
 
-			})
-		current_line.contract_line_id=contract_line_id
-		current_line.contract_id=contract_line_id.analytic_account_id
-		current_line.description=description
-		current_line.owner=owner
+# 			})
+# 		current_line.contract_line_id=contract_line_id
+# 		current_line.contract_id=contract_line_id.analytic_account_id
+# 		current_line.description=description
+# 		current_line.owner=owner
 
-		if related_user:
-			current_line.related_user_model=str(related_user._model)
-			current_line.related_user_id=related_user.id
-		else:
-			current_line.related_user_model=None
-			current_line.related_user_id=None
+# 		if related_user:
+# 			current_line.related_user_model=str(related_user._model)
+# 			current_line.related_user_id=related_user.id
+# 		else:
+# 			current_line.related_user_model=None
+# 			current_line.related_user_id=None
 
 
 
- 	#	pdb.set_trace()
+#  	#	pdb.set_trace()
 
-class lubon_qlan_users_o365(models.Model):
-	_inherit = 'lubon_qlan.users_o365'
-	billing_history_ids= fields.One2many('lubon_qlan.billing_history','related_user_id', domain=[('related_user_model','=','lubon_qlan.users_o365')])
+# class lubon_qlan_users_o365(models.Model):
+# 	_inherit = 'lubon_qlan.users_o365'
+# 	billing_history_ids= fields.One2many('lubon_qlan.billing_history','related_user_id', domain=[('related_user_model','=','lubon_qlan.users_o365')])
 
 
 class account_analytic_invoice_line(models.Model):
@@ -71,14 +71,14 @@ class account_analytic_invoice_line(models.Model):
 	line_ok=fields.Boolean(zcompute="_set_line_state")
 	usage_mandatory=fields.Boolean(default=True, help="Is usage required, -1 in current usage means not updated ?")
 	adaccount_ids=fields.One2many('lubon_qlan.adaccounts','contract_line_id')
-	billing_history_ids=fields.One2many('lubon_qlan.billing_history','contract_line_id')
+#	billing_history_ids=fields.One2many('lubon_qlan.billing_history','contract_line_id')
 #	counted_items=fields.Integer(compute="_count_items", string="Counted (old)", help="This number is the total of items counted in the tenant")
 #	used_items=fields.Integer(compute="_count_used", string="Used (old)", help="The number of items used/assigned")
-	billing_history_counted_items=fields.Integer(compute="_billing_history_count_items", string="Counted", store=True, help="This number is the total of items counted in the tenant", index=True)
+#	billing_history_counted_items=fields.Integer(compute="_billing_history_count_items", string="Counted", store=True, help="This number is the total of items counted in the tenant", index=True)
 	last_billed_usage=fields.Float(help="Last billed number")
 	current_usage=fields.Float(help="Current value of the counter, after billing it is set to -1", default=-1)
 	billing_check=fields.Boolean(compute="_calculate_billing_check", string="Billing check", store=True, index=True)
-	next_report_date=fields.Datetime(compute="_calculate_billing_check", help="Due date for the next reporting/invoicing", store=True)
+	next_report_date=fields.Datetime(compute="_calculate_next_report_date", help="Due date for the next reporting/invoicing", store=True)
 	source=fields.Char(help="Asset/product/user waar dit betrekking op heeft")
 
 
@@ -90,16 +90,20 @@ class account_analytic_invoice_line(models.Model):
 					line.billing_check=True
 				else:
 					line.billing_check=False
-				line.next_report_date=line.analytic_account_id.recurring_next_date	
+				
 
-	@api.multi
-	@api.depends('billing_history_ids')
-	def _billing_history_count_items(self):
+	# @api.multi
+	# @api.depends('billing_history_ids')
+	# def _billing_history_count_items(self):
+	# 	for line in self:
+	# 		#pdb.set_trace()
+	# 		line.billing_history_counted_items=len(line.billing_history_ids)
+
+	@api.one
+	@api.depends('analytic_account_id.recurring_next_date')
+	def _calculate_next_report_date(self):
 		for line in self:
-			#pdb.set_trace()
-			line.billing_history_counted_items=len(line.billing_history_ids)
-
-
+			line.next_report_date=line.analytic_account_id.recurring_next_date	
 
 	@api.one
 	@api.depends('name')
@@ -171,7 +175,7 @@ class account_analytic_account(models.Model):
 	quantity_hist=fields.Float(string="Historic balance", help="Credit before using odoo contracts")
 	partner_related_ids=fields.Many2many('res.partner')
 	date_cutoff=fields.Date(string="Last renewal date", help="Date to use as a start date for reporting and counting hours.")
-	billing_history_lines_ids=fields.One2many('lubon_qlan.billing_history','contract_id')
+	#billing_history_lines_ids=fields.One2many('lubon_qlan.billing_history','contract_id')
 	table_description=fields.Text(help = "Info die verschijnt op de contract table afdruk.")
 	
 	# @api.depends('recurring_next_date')
@@ -207,10 +211,10 @@ class account_analytic_account(models.Model):
 				if not(line.line_ok):
 					item.ready_for_invoice=False
 
-	@api.multi
-	def check_billing_lines(self,context):
-		for line in self.billing_history_lines_ids:
-			line.checkline()
+#	@api.multi
+#	def check_billing_lines(self,context):
+#		for line in self.billing_history_lines_ids:
+#			line.checkline()
 	
 	def _prepare_invoice_line(self, cr, uid, line, fiscal_position, context=None):
 		
